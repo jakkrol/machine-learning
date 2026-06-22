15248a8ff3ef86d34c4ec772c2693953a8096631
Jakub Król <kozak10555@gmail.com>
2026-06-22 21:14:05 +0200

    Refaktor: poprawne repozytorium bez wielkich plikow danych

--- pytorch-data/ep1/main.py
+++ pytorch-data/ep1/main.py
@@ -11,3 +11,55 @@ what_were_covering = {1: "data (prepare and load)",
     5: "saving and loading a model",
     6: "putting it all together"
 }
+
+# Create *known* parameters
+weight = 0.7
+bias = 0.3
+
+# Create data
+start = 0
+end = 1
+step = 0.02
+X = torch.arange(start, end, step).unsqueeze(dim=1)
+y = weight * X + bias
+
+#print(X[:10], y[:10])
+
+train_split = int(0.8 * len(X))
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+
+def plot_pr(train_data=X_train,
+            train_labels=y_train,
+            test_data=X_test,
+            test_labels=y_test,
+            predictions=None):
+    
+    plt.figure(figsize=(10,7))
+    plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+    plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+
+    if predictions is not None:
+        plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+    
+    plt.legend(prop={"size": 14});
+
+plot_pr()
+#plt.show()
+
+
+# Create a Linear Regression model class
+class LinearRegressionModel(nn.Module): # <- almost everything in PyTorch is a nn.Module (think of this as neural network lego blocks)
+    def __init__(self):
+        super().__init__() 
+        self.weights = nn.Parameter(torch.randn(1, # <- start with random weights (this will get adjusted as the model learns)
+                                                dtype=torch.float), # <- PyTorch loves float32 by default
+                                   requires_grad=True) # <- can we update this value with gradient descent?)
+
+        self.bias = nn.Parameter(torch.randn(1, # <- start with random bias (this will get adjusted as the model learns)
+                                            dtype=torch.float), # <- PyTorch loves float32 by default
+                                requires_grad=True) # <- can we update this value with gradient descent?))
+
+    # Forward defines the computation in the model
+    def forward(self, x: torch.Tensor) -> torch.Tensor: # <- "x" is the input data (e.g. training/testing features)
+        return self.weights * x + self.bias # <- this is the linear regression formula (y = m*x + b)
