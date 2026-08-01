26b517c198fcbeb69e3ab7042c77229a2805aa95
jakrol <kozak10555@gmail.com>
2026-08-01 17:09:11 +0200

    .:

--- pytorch-data/ep2/multi-class.py
+++ pytorch-data/ep2/multi-class.py
@@ -22,3 +22,66 @@ x_blob = torch.from_numpy(x_blob).type(torch.float)
 y_blob = torch.from_numpy(y_blob).type(torch.LongTensor)
 print(x_blob[:5], y_blob[:5])
 
+x_train, x_test, y_train, y_test = train_test_split(x_blob, y_blob, test_size=0.2, random_state=RANDOM_SEED)
+
+# plt.figure(figsize=(10, 7))
+# plt.scatter(x_blob[:, 0], x_blob[:, 1], c=y_blob, cmap=plt.cm.RdYlBu)
+# plt.show()
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {device}")
+
+class BlobModel(nn.Module):
+    def __init__(self, input_features, output_features, hidden_units=8):
+        super().__init__()
+        self.layer_stack = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=hidden_units),
+            #nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
+            #nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=output_features)
+        )
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.layer_stack(x)
+
+
+model = BlobModel(input_features=NUM_FEATURES, output_features=NUM_CLASSES, hidden_units=8).to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1)
+
+y_logits = model(x_train.to(device))
+y_preds_prob = torch.softmax(y_logits, dim=1)
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
+    acc = (correct / len(y_pred)) * 100 
+    return acc
+
+torch.manual_seed(42)
+epochs = 100
+
+x_train, y_train = x_train.to(device), y_train.to(device)
+x_test, y_test = x_test.to(device), y_test.to(device)
+
+for epoch in range(epochs):
+    model.train()
+
+    y_logits = model(x_train)
+    y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)
+    loss = loss_fn(y_logits, y_train)
+    acc = accuracy_fn(y_true=y_train, y_pred=y_pred)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    model.eval()
+    with torch.inference_mode():
+        test_logits = model(x_test)
+        test_pred = torch.softmax(test_logits, dim=1).argmax(dim=1)
+        test_loss = loss_fn(test_logits, y_test)
+        test_acc = accuracy_fn(y_true=y_test, y_pred=test_pred)
+
+    if epoch % 10 == 0:
+        print(f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Acc: {test_acc:.2f}%")
